b21fcd7fc46d1db360891a46cf3158b2714564c7
pedro-hsb <pedro.bernardo4561@gmail.com>
2024-09-29 23:40:30 -0300

    0.6.0 creation the routes and the verifications

--- src/main/java/com/fatec/controller/UserController.java
+++ src/main/java/com/fatec/controller/UserController.java
@@ -1,41 +1,65 @@
 package com.fatec.controller;
 
 
-import com.fatec.entity.User;
-import com.fatec.repository.AuthUserRepository;
+import com.fatec.model.User;
+import com.fatec.model.UserLogin;
+import com.fatec.repository.UserRepository;
+import com.fatec.service.UserService;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RequestMapping("/usuarios")
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @AllArgsConstructor
 public class UserController {
+    @Autowired
+    private UserService usuarioService;
 
-    private final AuthUserRepository userRepository;
-    private final PasswordEncoder passwordEncoder;
+    @Autowired
+    private UserRepository usuarioRepository;
 
     @GetMapping("/all")
     public ResponseEntity <List<User>> getAll(){
-        return ResponseEntity.ok(userRepository.findAll());
+
+        return ResponseEntity.ok(usuarioRepository.findAll());
+
     }
 
-    @PostMapping("/register")
-    public ResponseEntity registerUser(@RequestBody User user){
-        try {
-            if (userRepository.findByUsername(user.getUsername()).isPresent())
-                return ResponseEntity.status(HttpStatus.CONFLICT).body("Username already taken. Please try again");
-            user.setPassword(passwordEncoder.encode(user.getPassword()));
-            User save = userRepository.save(user);
-            return ResponseEntity.ok(HttpStatus.CREATED);
-        } catch (Exception e){
-            return ResponseEntity.internalServerError().body(e.getMessage());
-        }
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getById(@PathVariable Long id) {
+        return usuarioRepository.findById(String.valueOf(id))
+                .map(resposta -> ResponseEntity.ok(resposta))
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PutMapping("/atualizar")
+    public ResponseEntity<User> putUsuario(@Valid @RequestBody User usuario){
+        return usuarioService.atualizarUser(usuario)
+                .map(resposta -> ResponseEntity.status(HttpStatus.OK).body(resposta))
+                .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+    }
+
+    @PostMapping("/cadastrar")
+    public ResponseEntity<User> postUsuario(@Valid @RequestBody User usuario){
+        return usuarioService.cadastrarUser(usuario)
+                .map(resposta -> ResponseEntity.status(HttpStatus.CREATED).body(resposta))
+                .orElse(ResponseEntity.status(HttpStatus.BAD_REQUEST).build());
+    }
+
+    @PostMapping("/logar")
+    public ResponseEntity<UserLogin> autenticarUsuario(@Valid @RequestBody Optional<UserLogin> usuarioLogin){
+        return usuarioService.autenticarUser(usuarioLogin)
+                .map(resposta -> ResponseEntity.status(HttpStatus.OK).body(resposta))
+                .orElse(ResponseEntity.status(HttpStatus.UNAUTHORIZED).build());
     }
 
     @GetMapping("/test")
@@ -43,26 +67,3 @@ public class UserController {
         return ResponseEntity.status(HttpStatus.OK).body("Hello World");
     }
 }
-
-
-/*    @PostMapping("/cadastrar")
-    public ResponseEntity<User> postUsuario(@Valid @RequestBody User usuario){
-        return usuarioService.cadastrarUsuario(usuario)
-                .map(resposta -> ResponseEntity.status(HttpStatus.CREATED).body(resposta))
-                .orElse(ResponseEntity.status(HttpStatus.BAD_REQUEST).build());
-    }
-
-    @PutMapping("/atualizar")
-    public ResponseEntity<User> putUsuario(@Valid @RequestBody User usuario){
-        return usuarioService.atualizarUsuario(usuario)
-                .map(resposta -> ResponseEntity.status(HttpStatus.OK).body(resposta))
-                .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
-    }
-
-    @PostMapping("/logar")
-    public ResponseEntity<UsuarioLogin> autenticarUsuario(@Valid @RequestBody Optional<UsuarioLogin> usuarioLogin){
-        return usuarioService.autenticarUsuario(usuarioLogin)
-                .map(resposta -> ResponseEntity.status(HttpStatus.OK).body(resposta))
-                .orElse(ResponseEntity.status(HttpStatus.UNAUTHORIZED).build());
-    }
-*/
