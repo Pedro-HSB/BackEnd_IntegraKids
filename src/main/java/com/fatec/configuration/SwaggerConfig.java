9925e23753a2ab98282ef7b6c52fb4c6a1d09d12
pedro-hsb <pedro.bernardo4561@gmail.com>
2024-09-29 23:36:22 -0300

    0.2.0 create findByEmail

---
+++ src/main/java/com/fatec/configuration/SwaggerConfig.java
@@ -0,0 +1,4 @@
+package com.fatec.configuration;
+
+public class SwaggerConfig {
+}
---
+++ src/main/java/com/fatec/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.fatec.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.fatec.model.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+
+    Optional<User> findByEmail(String email);
+}
