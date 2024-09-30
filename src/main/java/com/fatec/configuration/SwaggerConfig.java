87f2c06d03fa751e735300b355ab52e1ed537ec4
pedro-hsb <pedro.bernardo4561@gmail.com>
2024-09-29 23:41:05 -0300

    0.7.0 implementation the swagger and the documentation

--- src/main/java/com/fatec/configuration/SwaggerConfig.java
+++ src/main/java/com/fatec/configuration/SwaggerConfig.java
@@ -1,4 +1,65 @@
 package com.fatec.configuration;
 
+
+import org.springdoc.core.customizers.OpenApiCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.responses.ApiResponse;
+import io.swagger.v3.oas.models.responses.ApiResponses;
+
+@Configuration
 public class SwaggerConfig {
+
+    @Bean
+    OpenAPI springBlogPessoalOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("IntegraKids")
+                        .description("Projeto Jornada da inclusao")
+                        .version("v0.0.1")
+                        .license(new License()
+                                .name("Projeto Jornada da inclusao")
+                                .url("https://github.com/Pedro-HSB"))
+                        .contact(new Contact()
+                                .name("Projeto Jornada da inclusao")
+                                .url("https://github.com/Pedro-HSB")
+                                .email("pedro.bernardo4561@gmail.com")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("Github")
+                        .url("https://github.com/Pedro-HSB/BackEnd_IntegraKids"));
+    }
+
+
+    @Bean
+    OpenApiCustomizer customerGlobalHeaderOpenApiCustomiser() {
+
+        return openApi -> {
+            openApi.getPaths().values().forEach(pathItem -> pathItem.readOperations().forEach(operation -> {
+
+                ApiResponses apiResponses = operation.getResponses();
+
+                apiResponses.addApiResponse("200", createApiResponse("Sucesso!"));
+                apiResponses.addApiResponse("201", createApiResponse("Objeto Persistido!"));
+                apiResponses.addApiResponse("204", createApiResponse("Objeto Excluído!"));
+                apiResponses.addApiResponse("400", createApiResponse("Erro na Requisição!"));
+                apiResponses.addApiResponse("401", createApiResponse("Acesso Não Autorizado!"));
+                apiResponses.addApiResponse("403", createApiResponse("Acesso Proibido!"));
+                apiResponses.addApiResponse("404", createApiResponse("Objeto Não Encontrado!"));
+                apiResponses.addApiResponse("500", createApiResponse("Erro na Aplicação!"));
+
+            }));
+        };
+    }
+
+    private ApiResponse createApiResponse(String message) {
+
+        return new ApiResponse().description(message);
+
+    }
 }
